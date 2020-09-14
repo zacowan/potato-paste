9cfb76bfb78172520eb2b609635d92c77e8f411e
Zachary Cowan <cz8ch8ry@gmail.com>
2020-09-14 00:20:27 -0400

    feat(url): added prod api url

--- client/src/api/utils.ts
+++ client/src/api/utils.ts
@@ -1,5 +1,5 @@
 const DEV_API_URL = 'http://localhost:5001/potato-paste/us-central1/api';
-const PROD_API_URL = '';
+const PROD_API_URL = 'https://us-central1-potato-paste.cloudfunctions.net/api';
 
 export const API_URL =
   process.env.NODE_ENV === 'development' ? DEV_API_URL : PROD_API_URL;
